e78210e456240b86ac851de5af7fa9980bd7cfa5
avalanchy <avalanchy@gmail.com>
2018-11-24 20:13:28 +0100

    Set year autmatically with JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
+  var year = new Date().getFullYear();
+  document.getElementById('year').innerText = year.toString();
 });
